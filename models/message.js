a8ae242512d0a3cf83f3d61008323cdc79438a86
Faiyaaz <faiyaaz.haque90@gmail.com>
2022-12-24 23:09:34 -0500

    initial skeleton commit

---
+++ models/message.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    title: {type:String, required:true, maxLength: 100},
+    timestamp:{type:Date},
+    message:{type:String, required:true, maxLength: 1000},
+    author:{type:Schema.Types.ObjectId, ref: "User"}
+});
+
+
+
+module.exports = mongoose.model("Message", MessageSchema);
